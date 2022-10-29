246e06f45b84295bd0cfbeaaf0b515f20b47b894
Geoffrey Hayes <hayesgm@gmail.com>
2022-10-28 22:56:43 -0700

    Add Comp.vote
    This patch adds Comp.vote to our Extensions.

--- src/Extensions.ts
+++ src/Extensions.ts
@@ -5,7 +5,6 @@ export const extensions: Extension[] = [
     id: 'comet_migrator',
     name: 'Compound III Migrator',
     permissions: {
-      storage: '*',
       trx: [
         {
           contract: '$operator',
@@ -23,6 +22,16 @@ export const extensions: Extension[] = [
       ipfs: 'QmRe6VBCXE9Wwb5fKkynYxtqbZ6Af1i72mhRzyN95vhxMy',
       domain: 'comet-v2-migrator.infura-ipfs.io',
       path: '/embedded.html'
+    }
+  },
+  {
+    id: 'comp_vote',
+    name: 'Comp.vote',
+    permissions: {
+      sign: '*'
+    },
+    source: {
+      url: 'https://comp-vote-xi.vercel.app/'
     }
   }
 ];
