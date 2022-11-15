f695c46c6ca62055c5193e1af23ef83b70a56b54
Mykel Pereira <me@mykelpereira.com>
2022-11-14 19:02:28 -0600

    Update comet-migrator ipfs hash

--- src/Extensions.ts
+++ src/Extensions.ts
@@ -26,7 +26,7 @@ export const extensions: Extension[] = [
       ]
     },
     source: {
-      ipfs: 'QmViFX6pTjtQQZG5y5ssBQNKUihr6CbHh3BJ6XCn1mTPzA',
+      ipfs: 'QmawBUEQ4nrj14gBBuQRWTyWapCpgoNQvPXwr1cysjcWEd',
       domain: 'comet-v2-migrator.infura-ipfs.io',
       path: '/embedded.html'
     },
